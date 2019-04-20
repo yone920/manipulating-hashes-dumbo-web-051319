d7b0dd6222e226fd12b6520aeaeee4978d09f87e
Yonas Fesehatsion <yone920@gmail.com>
2019-04-20 18:21:17 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -13,14 +13,3 @@ def second_challenge
   
 end
 
-
-groceries = {
-  dairy: ["milk", "yogurt", "cheese"],
-  vegetable: ["carrots", "broccoli", "cucumbers"],
-  meat: ["chicken", "steak", "salmon"],
-  grains: ["rice", "pasta"]
- }
-
-def get_the_values(groceries)
-  groceries.values.flatten
-end
